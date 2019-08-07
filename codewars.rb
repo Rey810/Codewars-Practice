228c3c783a3a30edb9d9b1ece5b9d777276873ec
ReyvdB <39665694+Rey810@users.noreply.github.com>
2019-08-07 19:03:20 +0200

    add encrypt this

--- codewars.rb
+++ codewars.rb
@@ -179,3 +179,38 @@ end
 def song_decoder(song)
   song.gsub("WUB", " ").gsub(/\s+/, " ").strip
 end
+
+
+
+
+#Encrypt this!
+=begin
+    Your message is a string containing space separated words.
+    You need to encrypt each word in the message using the following rules:
+        The first letter needs to be converted to its ASCII code.
+        The second letter needs to be switched with the last letter
+    Keepin' it simple: There are no special characters in input.
+=end
+
+def encrypt_this(text)
+  x = text.split(" ")
+  x.map! do |word, index|
+    if x[0].length == 1 || word.length == 1
+      "#{word[0].codepoints.join()}"
+    elsif word.length == 2
+      "#{word[0].codepoints.join()}#{word[1]}"
+    else
+    "#{word[0].codepoints.join()}#{word[-1]}#{word[2..-2]}#{word[1]}"
+    end
+  end
+  x.join(" ")
+end
+
+#Refactored to:
+def encrypt_this(text)  
+  text.split.map { |word|
+    word[-1], word[1] = word[1], word[-1] if word.size > 2
+    word[0] = word[0].ord.to_s
+    word
+    } .join(" ")
+end
